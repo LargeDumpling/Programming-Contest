fda991d29db50531fe122db944163228c5719138
LargeDumpling <LargeDumpling@qq.com>
2017-10-19 20:50:06 +0800

    Corrected some previous problems
    默认使用小于号进行比较
    默认大根堆

--- Templates/Heap/Mergeable Heap/code.cpp
+++ Templates/Heap/Mergeable Heap/code.cpp
@@ -1,84 +1,46 @@
-int key[100050],right[100050],left[100050],dis[100050];
-int merge(int A,int B)
-{//新结点记得将right和left值赋为0 
-	if(!A)
-		return B;
-	if(!B)
-		return A;
-	if(key[A]<key[B])
-	{
-		A+=B;
-		B=A-B;
-		A=A-B;
-	}
-	right[A]=merge(right[A],B);
-	if(dis[right[A]]>dis[left[A]])
-	{
-		right[A]+=left[A];
-		left[A]=right[A]-left[A];
-		right[A]=right[A]-left[A];
-	}
-	dis[A]=right[A]==0?0;dis[right[A]]+1;
-	return A;
-}
-struct heap
+template<typename Ty>
+struct jp
 {
-	int top,L[10050],R[10050],dist[10050];
-	struct node
+	static const int MAXN=100050;
+	int L[MAXN],R[MAXN],dist[MAXN],tOp,sz;
+	Ty data[MAXN];
+	jp()
 	{
-		int data,pos;
-		node()
-		{
-			data=pos=0;
-		}
-		bool operator<(const node x)const
-		{
-			return data<x.data;
-		}
-	}no[10050];
-	heap()
-	{
-		top=0;
+		tOp=sz=0;
 		memset(L,0,sizeof(L));
-		memset(R,0,sizoef(R));
+		memset(R,0,sizeof(R));
 		memset(dist,0,sizeof(dist));
 	}
+	void clear()
+	{
+		tOp=sz=0;
+		memset(L,0,sizeof(L));
+		memset(R,0,sizeof(R));
+		memset(dist,0,sizeof(dist));
+		return;
+	}
+	bool empty() { return (bool)tOp; }
 	int merge(int A,int B)
 	{
-		if(!A)
-			return B;
-		else if(!B)
-			return A;
-		if(B<A)
-			swap(A,B);
+		if(!A) return B;
+		if(!B) return A;
+		if(data[A]<data[B]) swap(A,B);
 		R[A]=merge(R[A],B);
-		if(dist[L[A]]<dist[R[A]])
-			swap(A,B);
+		if(dist[L[A]]<dist[R[A]]) swap(L[A],R[A]);
 		dist[A]=R[A]?dist[R[A]]+1:0;
 		return A;
 	}
-	void push(int data,int num)
+	void push(Ty X)
 	{
-		no[num]=jp(data,num);
-		top=merge(num,top);
+		data[++sz]=X;
+		tOp=merge(tOp,sz);
 		return;
 	}
 	void pop()
 	{
-		top=merge(L[top],R[top]);
+		tOp=merge(L[tOp],R[tOp]);
 		return;
 	}
-	jp top()
-	{
-		return node[top];
-	}
-}h;
-int main()
-{
-	freopen("code.in","r",stdin);
-	freopen("code.out","w",stdout);
-	fclose(stdin);
-	fclose(stdout);
-	return 0;
-}
+	Ty top() { return data[tOp]; }
+};
 
