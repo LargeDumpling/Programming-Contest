df9c48e26f03e2139f75042d266a909cf9f8c1ac
LargeDumpling <LargeDumpling@qq.com>
2017-10-20 00:32:19 +0800

    斜堆模板更新、增加一道比赛题目
    1、斜堆代码加入内存回收；
    2、增加2016~2017 CCPC final B题（HDU 6000）

--- Templates/Heap/Mergeable Heap/code.cpp
+++ Templates/Heap/Mergeable Heap/code.cpp
@@ -1,22 +1,29 @@
 template<typename Ty>
-struct jp
+struct heap
 {
 	static const int MAXN=100050;
-	int L[MAXN],R[MAXN],dist[MAXN],tOp,sz;
+	int tOp;
+	int L[MAXN],R[MAXN],dist[MAXN],p[MAXN];
 	Ty data[MAXN];
-	jp()
+	heap()
 	{
-		tOp=sz=0;
+		tOp=0;
 		memset(L,0,sizeof(L));
 		memset(R,0,sizeof(R));
 		memset(dist,0,sizeof(dist));
+		for(int i=1;i<MAXN;i++)
+			p[i]=i;
+		p[0]=MAXN-1;
 	}
 	void clear()
 	{
-		tOp=sz=0;
+		tOp=0;
 		memset(L,0,sizeof(L));
 		memset(R,0,sizeof(R));
 		memset(dist,0,sizeof(dist));
+		for(int i=1;i<MAXN;i++)
+			p[i]=i;
+		p[0]=MAXN-1;
 		return;
 	}
 	bool empty() { return (bool)tOp; }
@@ -26,19 +33,22 @@ struct jp
 		if(!B) return A;
 		if(data[A]<data[B]) swap(A,B);
 		R[A]=merge(R[A],B);
-		if(dist[L[A]]<dist[R[A]]) swap(L[A],R[A]);
+		if(dist[L[A]]<dist[R[A]])
+			swap(L[A],R[A]);
 		dist[A]=R[A]?dist[R[A]]+1:0;
 		return A;
 	}
 	void push(Ty X)
 	{
-		data[++sz]=X;
-		tOp=merge(tOp,sz);
+		data[p[p[0]]]=X;
+		tOp=merge(tOp,p[p[0]--]);
 		return;
 	}
 	void pop()
 	{
+		p[++p[0]]=tOp;
 		tOp=merge(L[tOp],R[tOp]);
+		L[p[p[0]]]=R[p[p[0]]]=0;
 		return;
 	}
 	Ty top() { return data[tOp]; }
