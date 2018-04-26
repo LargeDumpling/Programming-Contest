f506f38a9c8c3508664895c919d3d9d8b5645d31
LargeDumpling <LargeDumpling@qq.com>
2018-04-26 10:03:52 +0800

    Rearrange

---
+++ Algorithms&Data Structures/网络流/Dinic.cpp
@@ -0,0 +1,65 @@
+const int MAXN=3050;
+const int MAXM=4000050;
+const int INF=1000000050;
+int fir[MAXN],cur[MAXN],lev[MAXN],end[MAXM],next[MAXM],f[MAXM],ed,S,T;
+void addedge(int u,int v,int cap)
+{
+	end[++ed]=v;
+	next[ed]=fir[u];
+	fir[u]=ed;
+	f[ed]=cap;
+	end[++ed]=u;
+	next[ed]=fir[v];
+	fir[v]=ed;
+	f[ed]=0;
+	return;
+}
+bool BFS()
+{
+	int u;
+	memset(lev,-1,sizeof(lev));
+	queue<int>q;
+	lev[S]=0;
+	q.push(S);
+	while(q.size())
+	{
+		u=q.front(); q.pop();
+		for(int i=fir[u];i;i=next[i]) if(f[i]&&lev[end[i]]==-1)
+		{
+			lev[end[i]]=lev[u]+1;
+			q.push(end[i]);
+		}
+	}
+	memcpy(cur,fir,sizeof fir);
+	return lev[T]!=-1;
+}
+int DFS(int u,int maxf)
+{
+	if(u==T||!maxf) return maxf;
+	int cnt=0;
+	for(int &i=cur[u],tem;i;i=next[i]) if(f[i]&&lev[end[i]]==lev[u]+1)
+	{
+		tem=DFS(end[i],min(maxf,f[i]));
+		maxf-=tem;
+		f[i]-=tem;
+		f[i^1]+=tem;
+		cnt+=tem;
+		if(!maxf) break;
+	}
+	if(!cnt) lev[u]=-1;
+	return cnt;
+}
+int Dinic()
+{
+	int ans=0;
+	while(BFS())
+		ans+=DFS(S,2147483647);
+	return ans;
+}
+void init(int SS,int TT)
+{
+	memset(fir,0,sizeof(fir));
+	ed=1;
+	S=SS; T=TT;
+	return;
+}
