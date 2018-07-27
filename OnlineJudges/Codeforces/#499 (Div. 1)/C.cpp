308ccb371d714fc61e866690e75d8e0f7a3ab2d6
LargeDumpling <LargeDumpling@qq.com>
2018-07-27 21:04:30 +0800

    Codeforces Round #499 (Div. 1)

--- OnlineJudges/Codeforces/#499 (Div. 1)/C.cpp
+++ OnlineJudges/Codeforces/#499 (Div. 1)/C.cpp
@@ -30,22 +30,18 @@ int gcd(int A,int B)
 int main()
 {
 	memset(vis,false,sizeof(vis));
+	int om;
 	scanf("%d%d",&n,&m);
+	om=m;
 	for(int i=1;i<=n;i++)
 	{
 		scanf("%d",&a[i]);
-		a[i]=gcd(a[i],m);
+		m=gcd(a[i],m);
 	}
-	sort(a+1,a+n+1);
-	n=unique(a+1,a+n+1)-a-1;
-	for(int i=1;i<=n;i++)
-	{
-		if(a[i]==0) vis[0]=true;
-		else for(int j=0;j<m;j+=a[i])
-			vis[j]=true;
-	}
+	for(int j=0;j<om;j+=m)
+		vis[j]=true;
 	ans[0]=0;
-	for(int i=0;i<m;i++) if(vis[i])
+	for(int i=0;i<om;i++) if(vis[i])
 		ans[++ans[0]]=i;
 	printf("%d\n",ans[0]);
 	for(int i=1;i<=ans[0];i++)
